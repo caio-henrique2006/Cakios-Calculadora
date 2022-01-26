5caf36289fbc098a41da1a34b2e03cd1be102955
caio-henrique2006 <74103264+caio-henrique2006@users.noreply.github.com>
2022-01-25 21:27:37 -0300

    25/01/2022 finish modelo estático e start modelo interativo

--- cakios-calculator/src/index.js
+++ cakios-calculator/src/index.js
@@ -10,94 +10,126 @@ class Calculator extends React.Component {
 			this.handleEqualButton = this.handleEqualButton.bind(this);
 			this.handleACButton = this.handleACButton.bind(this);
 			// States:
-			this.state = {}
+			this.state = {
+				showingInScreen: " ",
+				outOfScreen: 0,
+			}
 		}
 	// Cuida dos botões número:
 	handleNumericButtons(number) {
-		return(
-			console.log(number)
-		)
+		const newResponse = this.state.showingInScreen + number;
+		this.setState({showingInScreen: newResponse})
 	}
 	// Cuida dos botões matemáticos:
 	handleMathButtons(simbol) {
-		return(
-			console.log(simbol)
-		)
+		const convertToInt = parseInt(this.state.showingInScreen);
+		const outOfScreen = this.state.outOfScreen;
+		switch (simbol) {
+			case "+":
+			console.log(convertToInt + outOfScreen);
+				this.setState({outOfScreen: convertToInt + outOfScreen});
+			break;
+			case "-":
+				console.log(convertToInt - outOfScreen);
+				this.setState({outOfScreen: outOfScreen - convertToInt});
+			break;
+			case "*":
+				if(outOfScreen === 0) {
+					// Caso a multiplicação seja 0 * convertToInt:
+					console.log(convertToInt * 1);
+					this.setState({outOfScreen: convertToInt * 1});
+				}else {
+					console.log(convertToInt * outOfScreen);
+					this.setState({outOfScreen: convertToInt * outOfScreen});
+				}
+			break;
+			case "/":
+				console.log(convertToInt / outOfScreen);
+				this.setState({outOfScreen: outOfScreen / convertToInt});
+			break;
+
+		}
+		this.setState({showingInScreen: " "})
 	}
 	// Cuida do botão equal =:
 	handleEqualButton() {
-
+		this.setState({showingInScreen: this.state.outOfScreen});
 	}
 	// Cuida do botão AC:
 	handleACButton() {
-
+		this.setState({showingInScreen: " "});
+		this.setState({outOfScreen: 0});
 	}
 	render() {
 		return(
 			<div className = "allScreen">
-				<div>
-					<ResponseScreen content="funcionou !!!"/>
+				{/* Bordas */}
+				<div className = "screenBorder">
 				</div>
-				{/* Butões de calculo + - x / */}
-				<div>
-					<button onClick = {() => this.handleMathButtons("+")}>
-						+
-					</button>
-					<button onClick = {() => this.handleMathButtons("-")}>
-						-
-					</button>
-					<button onClick = {() => this.handleMathButtons("*")}>
-						x
-					</button>
-					<button onClick = {() => this.handleMathButtons("/")}>
-						/
-					</button>
+				<div className = "calculatorBorder">
 				</div>
-				{/* Butões númericos de 0 a 9 */}
-				<div>
-					<button onClick = {() => this.handleNumericButtons(0)}>
-						0
-					</button>
-					<button onClick = {() => this.handleNumericButtons(1)}>
-						1
-					</button>
-					<button onClick = {() => this.handleNumericButtons(2)}>
-						2
-					</button>
-					<button onClick = {() => this.handleNumericButtons(3)}>
-						3
-					</button>
-					<button onClick = {() => this.handleNumericButtons(4)}>
-						4
-					</button>
-					<button onClick = {() => this.handleNumericButtons(5)}>
-						5
-					</button>
-					<button onClick = {() => this.handleNumericButtons(6)}>
-						6
-					</button>
-					<button onClick = {() => this.handleNumericButtons(7)}>
-						7
-					</button>
-					<button onClick = {() => this.handleNumericButtons(8)}>
-						8
-					</button>
-					<button onClick = {() => this.handleNumericButtons(9)}>
-						9
-					</button>
-				</div>
-					{/* Botão de igual = */}
-				<div>
-					<button onClick = {() => this.handleEqualButton()}>
-						=
-					</button>
-				</div>
+					{/* Tela de resposta */}
+					<div className = "responseScreen">
+						<ResponseScreen content={this.state.showingInScreen}/>
+					</div>
 					{/* Botão AC */}
-				<div>
-					<button onClick = {() => this.handleACButton()}>
-						AC
-					</button>
-				</div>
+					<div className = "allACButton">
+						<button className = "ACButton" onClick = {() => this.handleACButton()}>
+							AC
+						</button>
+					</div>
+					{/* Butões de calculo + - x / */}
+					<div className = "allMathButtons">
+						<button className = "mathButtons" onClick = {() => this.handleMathButtons("+")}>
+							+
+						</button><br/>
+						<button className = "mathButtons" onClick = {() => this.handleMathButtons("-")}>
+							-
+						</button><br/>
+						<button className = "mathButtons" onClick = {() => this.handleMathButtons("*")}>
+							x
+						</button><br/>
+						<button className = "mathButtons" onClick = {() => this.handleMathButtons("/")}>
+							/
+						</button>
+					</div>
+					<div className = "allNumericButtons">
+						{/* Butões númericos de 0 a 9 */}
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("9")}>
+							9
+						</button>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("8")}>
+							8
+						</button>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("7")}>
+							7
+						</button><br/>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("6")}>
+							6
+						</button>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("5")}>
+							5
+						</button>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("4")}>
+							4
+						</button><br/>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("3")}>
+							3
+						</button>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("2")}>
+							2
+						</button>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("1")}>
+							1
+						</button><br/>
+						<button className = "numericButtons" onClick = {() => this.handleNumericButtons("0")}>
+							0
+						</button>
+						{/* Botão de igual = */}
+						<button className = "numericButtons" onClick = {() => this.handleEqualButton()}>
+							=
+						</button>
+					</div>
 			</div>
 		)
 	}
